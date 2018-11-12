874df2e2acc51f4a3500ef10bfadc89066e97912
Sergei Garcia Ortega <sgarcia.dev@gmail.com>
2018-11-11 19:58:53 -0600

    Modify fake data in reducer

--- src/redux/note-reducer.js
+++ src/redux/note-reducer.js
@@ -8,12 +8,31 @@ const initialState = {
       user: {
         name: "Sergei Garcia"
       }
+    },
+    {
+      id: 456,
+      title: "How to learn CSS",
+      content: "Step 1 - Learn CSS.",
+      updateDate: new Date().toISOString(),
+      user: {
+        name: "Sergei Garcia"
+      }
+    },
+    {
+      id: 789,
+      title: "How to Graduate",
+      content:
+        "Step 1 - Learn to code. Step 2 - Build Capstones. Step 3 - Graduate.",
+      updateDate: new Date().toISOString(),
+      user: {
+        name: "Sergei Garcia"
+      }
     }
   ],
   noteDetails: {
     id: 123,
-    title: "How to learn ReactJS",
-    content: "Step 1 - Learn JavaScript. Step 2 - Learn React.",
+    title: "Note Placeholder Title",
+    content: "Note Placeholder Content",
     updateDate: new Date().toISOString(),
     user: {
       name: "Sergei Garcia"
